f6f7fde5d264e1f0e43e59279b1a2e419af165b7
Lisa <lblunt02@gmail.com>
2025-01-07 13:18:59 -0500

    finished lesson 8 of Udemy course, Variables and Functions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module lblunt02/LisaLearning.git
+
+go 1.23.3
--- main.go
+++ main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println("Hello, world.")
+
+	var whatToSay string
+	var i int64
+
+	whatToSay = "Goodbye World"
+	fmt.Println(whatToSay)
+
+	i = 7
+
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+
+	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
 }
